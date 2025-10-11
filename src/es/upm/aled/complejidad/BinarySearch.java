3d6321034c17278fe9c473f60c2ffa1dc4151187
Paek4545 <taehyun.paek.lee@alumnos.upm.es>
2025-10-11 17:11:50 +0200

    Deberes de recursividad y BubbleSort
    Hoy día 11/10/2025 he conseguido implementar el método binarySearch mediante recursividad. Por otro lado, tambiñen he conseguido implementar el método bubbleSort que ordena los elementos de array de forma ordanada.

--- src/es/upm/aled/complejidad/BinarySearch.java
+++ src/es/upm/aled/complejidad/BinarySearch.java
@@ -3,9 +3,10 @@ package es.upm.aled.complejidad;
 public class BinarySearch {
 	
 	private static int binarySearch(int[] list, int lo, int hi, int value) {
-		do {
+		// Deberes: ¿Implementarlo con recursividad?
+	/*	do {
 			int me = (int) Math.floor(lo + (hi-lo)/2);
-			int v = list[me];
+			int v = list[me]; // O(1) --> Acceder la posición del array
 			if (v == value) {
 				return me;
 			} 
@@ -17,6 +18,23 @@ public class BinarySearch {
 			}
 		} while(lo<hi);
 		return -1;
+	} */
+	 // Código general
+		int me = (int) Math.floor(lo + (hi-lo)/2); // rango válido mientras que lo <= hi
+		int v = list[me];	
+	 // Caso base (2 casos base)
+		
+		if (lo>hi) {
+			return -1;
+		} else if (v == value) {
+			return me;
+		}
+	 // Paso recursivo
+		if (v > value) {
+			return binarySearch(list, lo, me, value);	
+		} else {
+			return binarySearch(list, me+1, hi, value);
+		}
 	}
 	
 	// Método fachada (facade)
@@ -24,7 +42,7 @@ public class BinarySearch {
 		return binarySearch(list, 0, list.length, value);
 	}
 	public static void main(String[] args) {
-		// Array ORDENADO
+		// Array ORDENADO, sino no funciona
 		int[] list = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31};
 		int value = 19;
 		int posicion = binarySearch(list, value);
--- src/es/upm/aled/complejidad/BubbleSort.java
+++ src/es/upm/aled/complejidad/BubbleSort.java
@@ -1,5 +1,23 @@
 package es.upm.aled.complejidad;
 
-public class BubbleSort {
+import java.util.Arrays;
 
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] array = {14,29,10,7,30,80,23,45,50,2,3,100,53};
+		bubbleSort(array);
+	}
+	
+	public static void bubbleSort(int[] arr) {
+		for (int i = 0; i<arr.length; i++) {
+			for (int j = 0; j<arr.length - i -1; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
 }
